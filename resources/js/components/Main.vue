7069df3d856dd1aed3ad350879c389815e18dcb1
nwrthstwrm <aleks81_78@mail.ru>
2025-04-01 01:23:34 +0300

    главная в процессе, я беру перерыв на истерику

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,73 +1,121 @@
 <script setup>
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
+import { ref, onMounted } from 'vue';
+import { Splide } from '@splidejs/splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
+const newsItems = ref([
+  { 
+    title: 'Feed The Gods out now!',
+    description: 'This day has finally come. Rise to greatness in Feed The G...',
+    image: '@/assets/images/main/gamescard/title1.png'
+  },  
+  { 
+    title: 'Feed The Gods Release Date',
+    description: 'The Ancient One stir, hungry for devotion and sacrifi...',
+    image: '@/assets/images/main/gamescard/title1.png'
+  },
+  {
+    title: 'Dev Diary #4 - Release Date and future plans!',
+    description: 'Wow, Its been a long time since we have been in tou...',
+    image: '@/assets/images/main/gamescard/title1.png'
+  },
+  {
+     title: 'The Playtests starts!', 
+     description: 'Visit the Feed The Gods Steam store page starting today...',
+     image: '@/assets/images/main/gamescard/title1.png'
+  },
+  {
+    title: 'Play FEED THE GODS on Steam Next Fest!',
+    description: 'The demo version of our chaotic party card battler Feed The Gods...',
+    image: '@/assets/images/main/gamescard/title1.png'
+  },
+]);
+
+
+const gameItems = ref([
+  { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
+  { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
+  { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
+]);
+
+onMounted(() => {
+  const splideElement = document.querySelector('.splide');
+  if (splideElement) {
+    new Splide(splideElement, {
+      type: 'loop',
+      height: '9rem',
+      trimSpace: 'move',
+      perPage: 3,
+      start: 0, 
+      pagination: false,
+      classes: {
+        pagination: 'splide__pagination your-class-pagination',
+        page: 'splide__pagination__page your-class-page',
+      },
+      breakpoints: {
+        640: {
+          height: '6rem',
+        },
+      },
+    }).mount();
+  } else {
+    console.error('[splide] .splide element not found.');
+  }
+});
 </script>
 
 <template>
-    <header>
-        <Header />
-    </header>
-    <main>
-      <section>
-        <h1>TEDRED GAMES</h1>
-        <p>TedRed is a small developer with big ambitions</p>
-        <p>We make games that spark your imagination like the games you played as a kid.</p>
-      </section>
+  <header>
+    <Header />
+  </header>
+  <main>
 
-      <section class="news">
-        <h2>News</h2>
-        <div class="news-container">
-          <div class="news-item" v-for="(news, index) in newsItems" :key="index">
-            <h3>{{ news.title }}</h3>
-            <p>{{ news.description }}</p>
-            <button @click="readMore(index)">Show all</button>
-          </div>
+    <h1>В ПРОЦЕССЕ</h1>
+    <section class="mainTitleBlock">
+      <section class="title-wrapper">
+        <p class="quote quote-left">"</p>
+        <div class="title-content">
+          <p class="mainTitleText"><span>TedRed</span> is a small developer with big ambitions</p>
+          <p class="supportMainTitleText">We make games that spark your imagination like the games you played as a kid.</p>
         </div>
+        <p class="quote quote-right">"</p>
       </section>
+    </section>
 
-      <section class="games">
-        <h2>Our games</h2>
-        <div class="game-item" v-for="(game, index) in gameItems" :key="index">
-          <div class="game-image"></div>
-          <h3>{{ game.title }}</h3>
-          <p>{{ game.description }}</p>
-          <p>Available on</p>
-          <button @click="viewGame(index)">Game page</button>
-        </div>
-      </section>
-    </main>
 
-    <footer>
-        <Footer />
-    </footer>  
+    <p class="titleText">News</p>
+    <section class="splide" aria-label="Slide Container Example">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide" v-for="(item, index) in newsItems" :key="index">
+            <div class="splide__slide__container">
+              <img :src="item.image" alt="News Image">
+            </div>
+            <h3>{{ item.title }}</h3>
+            <p>{{ item.description }}</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="games">
+      <h2>Our games</h2>
+      <div class="game-item" v-for="(game, index) in gameItems" :key="index">
+        <div class="game-image"></div>
+        <h3>{{ game.title }}</h3>
+        <p>{{ game.description }}</p>
+        <p>Available on</p>
+        <button @click="viewGame(index)">Game page</button>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <Footer />
+  </footer>  
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      newsItems: [
-        { title: 'Title 1', description: 'Description text 1' },
-        { title: 'Title 2', description: 'Description text 2' },
-        { title: 'Title 3', description: 'Description text 3' },
-      ],
-      gameItems: [
-        { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
-        { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
-        { title: 'Feed The Gods', description: 'Feed The Gods is a casual card game...' },
-      ],
-    };
-  },
-  methods: {
-    readMore(index) {
-      // Логика для отображения полной информации о новости
-    },
-    viewGame(index) {
-      // Логика для перехода на страницу игры
-    },
-  },
-};
-</script>
 
 <style scoped>
 @font-face {
@@ -82,16 +130,71 @@ export default {
 main {
     display: block;
     font-family: Lexend;
+    color: white;
 }
 
-.header {
-  margin-bottom: 20px;
+.mainTitleBlock {
+  text-align: center;
+  margin-top: 8%;
+  position: relative;
 }
 
-.news-container, .game-item {
+.title-wrapper {
   display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+
+.mainTitleBlock .mainTitleText {
+  font-size: 2.7rem;
+  font-family: Molot;
+  margin: 0;
+}
+
+.mainTitleBlock .mainTitleText span {
+  color: #FFC851;
+}
+
+.mainTitleBlock .supportMainTitleText {
+  font-size: 1.2rem;
+  color: #8F8F8F;
+}
+
+.quote {
+  font-size: 128px; 
+  color: #AFAFAF;
+  line-height: 0.5;
+  margin: 0;
+}
+
+.quote-left {
+  float: left;
+  margin-right: 10px;
+  margin-bottom: 50px;
+}
+
+.quote-right {
+  float: right;
+  margin-left: 10px; 
+  margin-top: 100px;
+}
+
+.titleText {
+  font-family: Molot;
+  font-size: 64px;
+  text-align: center;
+}
+
+.news-container {
+  display: flex;
+  align-items: center;
   justify-content: center;
   margin: 10px 0;
+}
+
+.news-items {
+  display: flex;
 }
 
 .news-item {
@@ -100,6 +203,7 @@ main {
   padding: 10px;
   border-radius: 5px;
   width: 200px;
+  text-align: center;
 }
 
 .game-item {
@@ -108,6 +212,7 @@ main {
   padding: 10px;
   border-radius: 5px;
   width: 60%;
+
   height: 300px;
 }
 
@@ -123,5 +228,55 @@ main {
   justify-content: center;
   flex-direction: column;
   gap: 50px;
+  margin-top: 100px;
+}
+
+.splide {
+  margin: 20px auto;
+  max-width: 800px; 
+  margin-top: 100px;
+}
+
+.splide__slide {
+  background: rgba(0, 0, 0, 0.8);
+  padding: 20px; 
+  color: white;
+  text-align: center;
+  transition: transform 0.3s;
+}
+
+.splide__slide:hover {
+  transform: scale(1.05);
+}
+
+.splide__slide img {
+  width: 100%;
+  border-radius: 10px; 
+  margin-bottom: 10px; 
+}
+
+.splide__arrow {
+  color: white;
+  border: none;
+  width: 40px;
+  height: 40px;
+  display: flex; 
+  align-items: center;
+  justify-content: center; 
+  padding: 60px;
+}
+
+.splide__arrow:hover {
+  background: rgba(255, 255, 255, 0.8);
+}
+
+@media (max-width: 768px) {
+  .quote {
+    font-size: 64px;
+  }
+  
+  .title-wrapper {
+    gap: 10px;
+  }
 }
 </style>
